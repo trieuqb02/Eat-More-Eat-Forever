4d213495498b24301f5a31e699e1c1cbe0ea6f88
FunGi <>
2025-06-10 16:51:13 +0700

    [FIX]: fix underscore

--- Backend/src/main/java/com/enotion/Backend/socketio/SnakeSocketHandler.java
+++ Backend/src/main/java/com/enotion/Backend/socketio/SnakeSocketHandler.java
@@ -260,6 +260,7 @@ public class SnakeSocketHandler {
                 socketIOClient.sendEvent(EventName.TIMEOUT_CONNECTION.name(), "time out");
             }
         };
+    }
 
     private void spawnPowerUp(SocketIOServer server, String roomId) {
         int delay = ThreadLocalRandom.current().nextInt(3000, 7000);
@@ -275,6 +276,5 @@ public class SnakeSocketHandler {
 
     private void resetScore(String playerId) {
         playerScores.put(playerId, 0);
-
     }
 }
